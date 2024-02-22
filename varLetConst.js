085d5605d02fffe1943d4f0140baa4ae6a7412e4
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-22 18:52:41 +0600

    Revising js concepts

---
+++ varLetConst.js
@@ -0,0 +1,25 @@
+const money = 25;
+// money = 50;
+
+const rich = money + 25;
+// console.log(rich);
+
+let count = 0;
+count = count + 10;
+// console.log(count);
+
+const numbers = [23, 4, 23, 12, 56];
+numbers[1] = 55;
+numbers.push(8, 9, 65);
+// console.log(numbers);
+
+const student = {
+    name: "Bird",
+    class: 12,
+}
+
+// console.log(student);
+
+function add(num1, num2){
+    
+}
