67903642b488f1dabdd5ca4abeeefca85eede750
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-26 12:31:58 +0600

    Learning Hoisting

---
+++ Understand-Common-Javascript-Concepts/triple.js
@@ -0,0 +1,33 @@
+const first = 2;
+const second = 2;
+// if(first == second){
+//     console.log("Values are equal");
+// }
+// else{
+//     console.log("Values are not equal");
+// }
+
+// if(first === second){
+//     console.log("Values are equal");
+// }
+// else{
+//     console.log("Values are not equal");
+// }
+
+function add(a, b){
+    const total = a + b;
+    // console.log(a, b);
+    if(b > 5){
+        const sum = 25 + a + b;
+    }
+    else{
+        const sum = 5 + a + b;
+        var current = sum;
+    }
+    // console.log(sum);
+    console.log(current);
+    return total;
+}
+
+// console.log(a, b);
+add(5, 3);
--- Understand-Common-Javascript-Concepts/types.js
+++ Understand-Common-Javascript-Concepts/types.js
@@ -1,7 +1,7 @@
-const a = 5;
+// const a = 5;
 // console.log(typeof a);
 
-const b = "Learning Web Development";
+// const b = "Learning Web Development";
 // console.log(typeof b);
 
 const ages = [45, 65, 48];
@@ -13,39 +13,39 @@ const student = {
 };
 // console.log(typeof student);
 
-const d = true;
+// const d = true;
 // console.log(typeof d);
 
-let x = 5;
-let y = x;
+// let x = 5;
+// let y = x;
 
 // console.log(x, y);
 
-y = 7;
+// y = 7;
 
 // console.log(x, y);
 
-let p = {
-    job: "Web Developer"
-};
-let q = p;
+// let p = {
+    // job: "Web Developer"
+// };
+// let q = p;
 
 // q = {
 //     job: "Backend web developer"
 // }
 
-q.job = "Front End Web Developer";
+// q.job = "Front End Web Developer";
 
 // console.log(p, q);
 
-let first;
+// let first;
 // console.log(first);
 
 function second(a, b){
     const total = a + b;
 }
 
-const result = second();
+// const result = second();
 // console.log(result);
 
 function third(a, b, c, d){
@@ -62,7 +62,7 @@ function noNegative(a, b){
     return a + b;
 }
 
-const total = noNegative(2, 5);
+// const total = noNegative(2, 5);
 // console.log(total);
 
 const fifth = {
@@ -89,4 +89,22 @@ const data = {
 }
 
 // console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof null);
+
+const x = false;
+// if(x){
+//     console.log("Value of x is truthy");
+// }
+// else{
+//     console.log("Value of x is falsy");
+// }
+
+const y = "";
+// if(!y){
+//     console.log("Value is falsy");
+// }
+
+const z = " ";
+if(!!z){
+    console.log("Value is truthy");
+}
