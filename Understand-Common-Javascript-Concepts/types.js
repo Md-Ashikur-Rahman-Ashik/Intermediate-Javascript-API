f63843cf49f7be4fe24139f7684c7904e49669a6
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-26 10:23:50 +0600

    Revising JS data types

---
+++ Understand-Common-Javascript-Concepts/types.js
@@ -0,0 +1,17 @@
+const a = 5;
+// console.log(typeof a);
+
+const b = "Learning Web Development";
+// console.log(typeof b);
+
+const ages = [45, 65, 48];
+// console.log(typeof ages);
+
+const student = {
+    id: 23,
+    class: 7
+};
+// console.log(typeof student);
+
+const d = true;
+console.log(typeof d);
