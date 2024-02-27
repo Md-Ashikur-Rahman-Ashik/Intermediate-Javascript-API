dabc6410aaa91bfffbbd18c8d13797a12591b2cb
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-27 14:53:02 +0600

    Learning JS HTTP methods

--- API-JSON-Data-Load-Dynamic-Website/posts.js
+++ API-JSON-Data-Load-Dynamic-Website/posts.js
@@ -1,6 +1,32 @@
 function loadPosts(){
     // console.log("Loading Posts Data");
     fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => displayPosts(data))
+}
+
+function displayPosts(data){
+    // console.log(data);
+    const containerPost = document.getElementById("postContainer");
+    for(const info of data){
+        console.log(info);
+        const div = document.createElement("div");
+        div.classList.add("post");
+        div.innerHTML = `
+            <h4>User-${info.userId}</h4>
+            <h5>Post: ${info.title}</h5>
+            <p>${info.body}</p>
+        `;
+        containerPost.appendChild(div);
+    }
+}
+
+loadPosts();
+
+function deletePost(){
+    fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "DELETE",
+    });
     
 }
-loadPosts();
