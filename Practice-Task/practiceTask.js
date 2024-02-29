b8840b3733b85eed9213dfe8dcc05e0d6f878fc7
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-29 13:06:06 +0600

    Solving practice tasks

---
+++ Practice-Task/practiceTask.js
@@ -0,0 +1,37 @@
+// function fiveSecondDelay(){
+//     console.log("Testing this function to be executed after five seconds.");
+// }
+
+// setTimeout(() => {
+//     console.log("Testing this function to be executed after five seconds");
+// }, 5000);
+
+// function delayedGreeting(name, time){
+//     console.log("Ahlan Sahlan", name);
+// }
+
+// setTimeout((delayedGreeting),time)
+
+// function delayGreeting(name, time){
+//     function delayedGreeting(){
+//         console.log("Ahlan Sahlan", name);
+//     }
+
+//     setTimeout(delayedGreeting, time);
+// }
+
+// delayGreeting("Alice", 2000);
+
+function tellJoke() {
+  console.log(
+    "Why don't scientists trust atoms? Because they make up everything!"
+  );
+}
+
+const jokeInterval = setInterval(tellJoke, 2000);
+setTimeout(() => {
+    clearInterval(jokeInterval);
+}, 10000);
+
+
+// tellJoke();
