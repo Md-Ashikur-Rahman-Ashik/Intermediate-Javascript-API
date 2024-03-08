c0abf17f0fcac47d63315dc82e9228842b871f0d
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-08 15:28:05 +0600

    Making shopping cart functional

--- BONUS-LOCAL-STORAGE/cart.js
+++ BONUS-LOCAL-STORAGE/cart.js
@@ -3,6 +3,17 @@ const addProduct = () => {
     const quantityField = document.getElementById("productQuantity");
     const product = productField.value;
     const quantity = quantityField.value;
+    productField.value = "";
+    quantityField.value = "";
+
+    const containerOfProduct = document.getElementById("productContainer");
+    const li = document.createElement("li")
+    li.innerText = `Your desired product is ${product}`;
+    const secondLi = document.createElement("li");
+    secondLi.innerText = `You have added ${quantity} of it`;
+    containerOfProduct.appendChild(li);
+    containerOfProduct.appendChild(secondLi);
+
     console.log("The product is", product);
     console.log("The product quantity is", quantity);
 }
